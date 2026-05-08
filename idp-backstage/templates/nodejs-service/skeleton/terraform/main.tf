e6445554cdebd25cbe3dbc77a61331a8592a354e
ntrangng <23521628@gm.uit.edu.vn>
2026-05-08 10:53:59 +0700

    feat(phase2): implement OIDC authentication and AWS infrastructure automation

---
+++ idp-backstage/templates/nodejs-service/skeleton/terraform/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+resource "aws_instance" "app_server" {
+    ami = "ami-02dd44faa40720bb8"
+    instance_type = "t3.micro"
+
+    tags = {
+    Name        = "${{ values.name }}-server"
+    ManagedBy   = "GenAI-IDP-Terraform"
+    }
+  
+}
